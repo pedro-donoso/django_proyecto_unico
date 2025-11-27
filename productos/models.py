29bf5df5d9760f16b7b0e6589edc6e0459aba4ad
pedro-donoso <pedro.eduardo.donoso@gmail.com>
2025-11-27 13:48:18 -0300

    modifico models

--- productos/models.py
+++ productos/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Producto(models.Model):
-    nombre = models.CharField(max_length=100)
+    nombre = models.CharField(max_length=100, db_index=True)
     precio = models.DecimalField(max_digits=5, decimal_places=2)
     disponible = models.BooleanField(default=True)
 
